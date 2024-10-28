19abfa76f21db25828b2fb6a1c97ead4a8b1bdcc
yassineraddaoui <yassineraddaouicontact@gmail.com>
2024-10-28 12:23:38 +0100

    Owner Controller : processFindForm Test will answer general

--- src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -4,6 +4,7 @@ import guru.springframework.sfgpetclinic.fauxspring.BindingResult;
 import guru.springframework.sfgpetclinic.fauxspring.Model;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,10 +14,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
@@ -24,115 +25,96 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
 
-    public static final String FIND_OWNERS = "owners/findOwners";
-    public static final String OWNERS_LIST = "owners/ownersList";
+    private static final String FIND_OWNERS = "owners/findOwners";
+    private static final String OWNERS_LIST = "owners/ownersList";
     private static final String REDIRECT_OWNERS_1 = "redirect:/owners/1";
     private static final String VIEWS_OWNER_CREATE_OR_UPDATE_FORM = "owners/createOrUpdateOwnerForm";
+
     @Mock
     OwnerService service;
+
     @InjectMocks
     OwnerController controller;
+
     @Mock
     BindingResult bindingResult;
+
     @Mock
     Model model;
 
     @Captor
     ArgumentCaptor<String> stringArgumentCaptor;
 
+    @BeforeEach
+    void setUp() {
+        lenient().when(service.findAllByLastNameLike(stringArgumentCaptor.capture()))
+                .thenAnswer(invocation -> {
+                    String argument = stringArgumentCaptor.getValue();
+                    switch (argument) {
+                        case "%Doe%":
+                            return List.of(new Owner(1L, "John", "Doe"));
+                        case "%DontFindMe%":
+                            return Collections.emptyList();
+                        case "%FindMultiple%":
+                            return List.of(new Owner(1L, "John", "Doe"),
+                                    new Owner(2L, "John", "Doe"));
+                    }
+                    throw new RuntimeException("No case satisfied");
+                });
+    }
+
     @Test
-    void processCreationFormBDD() {
-        //given
+    void processCreationFormHasNoErrors() {
         Owner owner = new Owner(1L, "John", "Doe");
         given(bindingResult.hasErrors()).willReturn(false);
         given(service.save(any())).willReturn(owner);
-        //when
-        var result = controller.processCreationForm(owner, bindingResult);
-        //then
-        then(service).should(atMostOnce()).save(owner);
-        assertEquals(REDIRECT_OWNERS_1, result);
 
+        String result = controller.processCreationForm(owner, bindingResult);
+
+        then(service).should().save(owner);
+        assertThat(result).isEqualTo(REDIRECT_OWNERS_1);
     }
 
     @Test
-    void processCreationForm() {
-
+    void processCreationFormHasErrors() {
         Owner owner = new Owner(1L, "John", "Doe");
-        when(bindingResult.hasErrors()).thenReturn(false);
-        when(service.save(owner)).thenReturn(owner);
-        var result = controller.processCreationForm(owner, bindingResult);
-        assertEquals(REDIRECT_OWNERS_1, result);
-        verify(service, atLeastOnce()).save(owner);
+        given(bindingResult.hasErrors()).willReturn(true);
 
+        String result = controller.processCreationForm(owner, bindingResult);
+
+        assertThat(result).isEqualTo(VIEWS_OWNER_CREATE_OR_UPDATE_FORM);
+        then(service).should(never()).save(owner);
     }
 
     @Test
-    void processCreationFormHasError() {
+    @DisplayName("ProcessFindForm: Change argument before passing it to the service")
+    void processFindFormWithSingleOwner() {
         Owner owner = new Owner(1L, "John", "Doe");
-        when(bindingResult.hasErrors()).thenReturn(true);
-
-
-        var result = controller.processCreationForm(owner, bindingResult);
-        //then
-        assertEquals(VIEWS_OWNER_CREATE_OR_UPDATE_FORM, result);
-        verify(service, never()).save(owner);
-
-    }
-
-    @Test
-    @DisplayName("ProcessFindForm :Change argument before passing it to the service")
-    void processFindFormWildcard() {
-        Owner owner = new Owner(1L, "John", "Doe");
-        given(service.findAllByLastNameLike(stringArgumentCaptor.capture())).willReturn(List.of(owner));
 
         String result = controller.processFindForm(owner, bindingResult, model);
 
         assertThat(stringArgumentCaptor.getValue()).isEqualTo("%Doe%");
-    }
-
-
-    @Test
-    void processFindFormTest() {
-        Owner owner = new Owner(1L, "John", "Doe");
-        //given
-        when(service.findAllByLastNameLike(anyString())).thenReturn(List.of(owner));
-        //when
-
-        String result = controller.processFindForm(owner, bindingResult, model);
-        //then
-
-        assertEquals(REDIRECT_OWNERS_1, result);
-        verify(service).findAllByLastNameLike(anyString());
+        assertThat(result).isEqualTo(REDIRECT_OWNERS_1);
     }
 
     @Test
-    void processFindFormTestOwnerLastNameNull() {
-        Owner owner = new Owner(1L, "John", null);
-        //given
-        when(service.findAllByLastNameLike(anyString())).thenReturn(List.of());
-        //when
+    void processFindFormWithNoOwner() {
+        Owner owner = new Owner(1L, "John", "DontFindMe");
 
         String result = controller.processFindForm(owner, bindingResult, model);
-        //then
 
-        assertEquals(FIND_OWNERS, result);
-        verify(service).findAllByLastNameLike("%%");
+        assertThat(stringArgumentCaptor.getValue()).isEqualTo("%DontFindMe%");
+        assertThat(result).isEqualTo(FIND_OWNERS);
     }
 
     @Test
-    void processFindFormTestOwnerMultipleOwners() {
-        Owner owner = new Owner(1L, "John", "Doe");
-        Owner owner2 = new Owner(2L, "John", "Doe");
-
-        //given
-        when(service.findAllByLastNameLike(anyString())).thenReturn(List.of(owner, owner2));
-        //when
+    void processFindFormWithMultipleOwners() {
+        Owner owner = new Owner(1L, "John", "FindMultiple");
 
         String result = controller.processFindForm(owner, bindingResult, model);
-        //then
 
-        assertEquals(OWNERS_LIST, result);
-        verify(service).findAllByLastNameLike("%Doe%");
+        assertThat(result).isEqualTo(OWNERS_LIST);
+        assertThat(stringArgumentCaptor.getValue()).isEqualTo("%FindMultiple%");
     }
 
 }
